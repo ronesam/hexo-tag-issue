cf4a5f16a16531221f3b3db07118d0ae9bff3f5d
ronesam <ronesam@qq.com>
2015-12-04 23:30:51 +0800

    占位支持 issue。

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 'use strict';
 
-hexo.extend.tag.register('issue', function(args, content){
-  //第一次分析出整个问题&答案
-  //[s]+ 软件开发中需求工作的目的是
-  //[类型]顺序 标题
-  //题目类型: [s]单选 [m]多选
-  //选项顺序: +原始顺序 *随机组合
-  var issue = /\[([a-z]+)\][+*] ?[\s\S]*(\[|$)/g;
-
-},{ends: true});
+hexo.extend.tag.register('issue', require('./lib/tag'), true);
